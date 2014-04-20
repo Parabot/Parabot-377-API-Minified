8e6f645b050ada0079d34ce7c4bf27b841a47260
Clisprail <clisprail@parabot.org>
2014-04-20 14:12:26 +0200

    Fixed walking

--- src/org/rev377/min/api/methods/Walking.java
+++ src/org/rev377/min/api/methods/Walking.java
@@ -7,27 +7,39 @@ import org.rev377.min.api.wrappers.TilePath;
 /**
  * 
  * @author Everel
- *
+ * 
  */
 public class Walking {
-	
+
 	public static void walkTo(Tile from, Tile to) {
-		// Loader.getClient().walkTo(0, 0, 0, 0, from.getRegionY(), 0, 0, to.getRegionY(), from.getRegionX(), true, to.getRegionX());
-		Loader.getClient().walkTo(true, false, from.getRegionY(), to.getRegionY(), 0, 0, 1, 0, from.getRegionX(), 0, 0, to.getRegionX());
+		Loader.getClient().walkTo(true, false, to.getRegionY(),
+				from.getRegionY(), 0, 0, 0, 0, to.getRegionX(), 0, 0,
+				from.getRegionX());
 	}
-	
+
 	/**
 	 * 
 	 * @param tilePath
 	 * @return <b>true</b> if destination reached, otherwise <b>false</b>
 	 */
 	public static boolean walkDown(TilePath tilePath) {
-		if(tilePath.hasReached()) {
+		if (tilePath.hasReached()) {
 			return true;
 		}
 		tilePath.traverse();
 		return false;
 	}
 
-}
+	public static Tile getNearestTileTo(Tile tile) {
+		Tile loc = Players.getMyPlayer().getLocation();
+		for (int i = 0; i < 1000; ++i) {
+			if (tile.distanceTo() < 16 && tile.isWalkable()) {
+				return tile;
+			}
+			tile = new Tile((loc.getX() + tile.getX()) / 2,
+					(loc.getY() + tile.getY()) / 2);
+		}
+		return null;
+	}
 
+}

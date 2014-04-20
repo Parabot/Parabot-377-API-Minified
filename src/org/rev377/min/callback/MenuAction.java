6af9a5abc2932e73680ee4e09b3aa17b25b0d333
Clisprail <clisprail@parabot.org>
2014-04-20 14:12:07 +0200

    Added Walking intercepting Note: for developers only

--- src/org/rev377/min/callback/MenuAction.java
+++ src/org/rev377/min/callback/MenuAction.java
@@ -2,7 +2,10 @@ package org.rev377.min.callback;
 
 import org.rev377.min.Loader;
 import org.rev377.min.accessors.Client;
+import org.rev377.min.api.wrappers.Player;
 import org.rev377.min.debug.DActions;
+
+import org.rev377.min.api.methods.Players;
 
 /**
  * 
@@ -20,6 +23,13 @@ public class MenuAction {
 			int actionId = client.getMenuActionId()[index];
 			System.out.println(String.format("[index: %d, action1: %d, action2: %d, action3: %d, id: %d]", index, action1, action2, action3, actionId));
 		}
+	}
+	
+	// intercepts walking
+	public static void intercept(boolean paramBoolean1, boolean paramBoolean2, int paramInt1, int paramInt2, int paramInt3, int paramInt4, int paramInt5, int paramInt6, int paramInt7, int paramInt8, int paramInt9, int paramInt10) {
+		Player p = Players.getMyPlayer();
+		System.out.println(p.getLocation().getRegionX() + ", " + p.getLocation().getRegionY());
+		System.out.println(paramBoolean1 + ", " + paramBoolean2 + ", " + paramInt1 + ", " + paramInt2 + ", " + paramInt3 + ", " + paramInt4 + ", " + paramInt5 + ", " + paramInt6 + ", " + paramInt7 + ", " + paramInt8 + ", " + paramInt9 + ", " + paramInt10);
 	}
 
 }
